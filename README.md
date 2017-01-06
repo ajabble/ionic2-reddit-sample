4e9fb8d569ac90ba4841523c49c7309eb73d93ad
Ashish Jabble <ashish.jabble@nerdapplabs.com>
2017-01-06 13:28:57 +0530

    feeds are now filtered by category with limit from app itself

--- README.md
+++ README.md
@@ -4,10 +4,7 @@
 
 > ionic2 based app using Angular2 TypeScript
 
-> Demo app Fetching feeds from Reddit API using 2 parameters only  i.e category (sports) with limit(10)
-
-#### TODO
-> Will add dynamic category and limit stuff so that user can filter from app itself
+> Demo app Fetching feeds from Reddit API using 2 parameters only  i.e by category (sports) & by limit(10)
 
 #### Requirements
 
@@ -25,4 +22,4 @@
 3. If you want to run output on iOS simulator
    -  if any error related with platform, then do `cordova platform add ios --save`
    - `ionic run ios` => run on default iOS simuator
-   - `ionic emulate --target="iPhone7, 10.2"` => run on specific simulator, if multiple
+   - `ionic emulate --target="iPhone-7, 10.2"` => run on specific simulator, if multiple
--- src/pages/reddit/reddit.ts
+++ src/pages/reddit/reddit.ts
@@ -9,23 +9,41 @@ import {FeedDetailPage} from "../detail/feeddetail"
 })
 export class RedditPage {
   feedsArray:any;
+  filter:any;
+  limit:any;
 
   constructor(public navCtrl: NavController, private service:Service) {
-
+    this.getDefaults();
   }
 
+  // Default constructor
+  getDefaults(){
+    this.filter = 'sports';
+    this.limit = 10;
+  }
+
+  // Init Constructor
   ngOnInit(){
-    console.log('Called on init!');
-    this.getFeeds('sports', 15);
+    console.log('Called on init!', this.filter);
+    this.getFeeds(this.filter, this.limit);
   }
 
+  
+  // fetching feeds from API with filter
   getFeeds(category, limit){
     this.service.getFeeds(category, limit)
     .subscribe(response => this.feedsArray = response.data.children);
     //.subscribe(response => console.log(response)); // If you want to see response log
   }
 
+ // To see the feed detail object
   viewFeed(feed){
     this.navCtrl.push(FeedDetailPage, {feed:feed});
   }
+
+  // feeds filtered by
+  filterBy(){
+    this.getFeeds(this.filter, this.limit);
+  }
+
 }
